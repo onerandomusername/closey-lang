9f179b7ac6ae7b01329ee556d018366960b798fb
jenra <a>
2021-06-21 09:46:45 -0400

    Slight refactor

--- src/backends/x86/codegen.rs
+++ src/backends/x86/codegen.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use super::super::ir::{IrArgument, IrFunction, IrInstruction, IrModule};
+use super::super::common::{self, GeneratedCode};
 
 const NONARG_REGISTER_COUNT: usize = 9;
 
@@ -69,99 +70,6 @@ impl Register {
     }
 }
 
-#[derive(Default)]
-pub struct GeneratedCode {
-    func_addrs: HashMap<String, usize>,
-    func_refs: HashMap<usize, String>,
-    data: Vec<u8>
-}
-
-impl GeneratedCode {
-    pub fn new() -> GeneratedCode {
-        GeneratedCode {
-            func_addrs: HashMap::new(),
-            func_refs: HashMap::new(),
-            data: Vec::new()
-        }
-    }
-
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.data.is_empty()
-    }
-
-    pub fn as_ptr(&self) -> *const u8 {
-        self.data.as_ptr()
-    }
-
-    pub fn relocate(&mut self, base: *const u8) {
-        for (code_addr, func) in self.func_refs.iter() {
-            if let Some(offset) = self.func_addrs.get(func) {
-                let addr = base as usize + *offset;
-
-                for (i, byte) in self.data.iter_mut().skip(*code_addr).enumerate() {
-                    if i >= 8 {
-                        break;
-                    }
-
-                    *byte = ((addr >> (i * 8)) & 0xff) as u8;
-                }
-            }
-        }
-    }
-
-    #[allow(clippy::missing_safety_doc)]
-    pub unsafe fn get_main_fn(&self, base: *const u8) -> Option<extern "C" fn() -> u64> {
-        if let Some(f) = self.func_addrs.get("main") {
-            use std::mem::transmute;
-            Some(transmute(base.add(*f)))
-        } else {
-            None
-        }
-    }
-
-    pub fn print_data(&self) {
-        let mut i = 0;
-        for c in self.data.iter() {
-            print!("{:02x} ", c);
-            i += 1;
-            if i >= 16 {
-                i = 0;
-                println!();
-            }
-        }
-        if i != 0 {
-            println!();
-        }
-    }
-}
-
-fn linear_scan(func: &mut IrFunction, register_count: usize) {
-    let mut register_lifetimes = vec![0usize; register_count];
-
-    'a: for ssa in func.ssas.iter_mut() {
-        for lifetime in register_lifetimes.iter_mut() {
-            if *lifetime > 0 {
-                *lifetime -= 1;
-            }
-        }
-
-        if ssa.local.is_some() {
-            for (reg, lifetime) in register_lifetimes.iter_mut().enumerate() {
-                if *lifetime == 0 {
-                    *lifetime = ssa.local_lifetime;
-                    ssa.local_register = reg;
-                    continue 'a;
-                }
-            }
-            ssa.local_register = register_count;
-        }
-    }
-}
-
 pub fn generate_code(module: &mut IrModule) -> GeneratedCode {
     let mut code = GeneratedCode::new();
 
@@ -201,7 +109,7 @@ pub fn generate_code(module: &mut IrModule) -> GeneratedCode {
         // Add function
         code.func_addrs.insert(func.name.clone(), code.len());
 
-        linear_scan(func, NONARG_REGISTER_COUNT);
+        common::linear_scan(func, NONARG_REGISTER_COUNT);
 
         let mut local_to_register = HashMap::new();
         for ssa in func.ssas.iter() {
