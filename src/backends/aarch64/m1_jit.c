0cef649be650f826f427d6936f6b7e1ae7c83f78
jenra-uwu <a>
2021-07-07 18:34:25 -0400

    aaaaaaaaaa

--- src/backends/aarch64/m1_jit.c
+++ src/backends/aarch64/m1_jit.c
@@ -5,7 +5,7 @@
 
 #include "m1_jit.h"
 
-#if !defined(__APPLE__) && !defined(__aarch64__)
+#if !defined(__APPLE__) || !defined(__aarch64__)
 void pthread_jit_write_protect_np(int _) { if (_) {  } }
 #else
 void ________is_not_used_but_is_here_to_suppress_warnings________() { }
